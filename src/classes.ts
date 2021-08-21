6d22f2c998f554fab2c3ce9f0ee996849a60df92
CRBroughton <crbroughton@posteo.uk>
2021-08-21 11:04:35 +0100

    added Person class

---
+++ src/classes.ts
@@ -0,0 +1,19 @@
+// Classes
+
+class Person {
+    id: number
+    name: string
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    register() {
+        return `${this.name} is now registered`
+    }
+}
+
+const craig = new Person(1, 'craig');
+
+console.log(craig.register)
