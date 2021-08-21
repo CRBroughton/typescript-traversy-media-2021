092e5a2bb615c41d76b8a7583ad6118874112d2a
CRBroughton <crbroughton@posteo.uk>
2021-08-21 10:52:32 +0100

    added functiosn to functions.ts

---
+++ src/functions.ts
@@ -0,0 +1,13 @@
+// Functions
+
+
+// Returns type of number
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+// Return void
+function log(message: number | string): void {
+    console.log(log);
+}
+
